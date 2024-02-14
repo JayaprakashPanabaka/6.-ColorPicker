6b82966c71fef98f738a99cc815a420fc03113d6
JayaprakashPanabaka <jpanabaka1729@gmail.com>
2024-02-14 17:40:19 +0530

    First commit

---
+++ index.js
@@ -0,0 +1,31 @@
+let bgContainerEl = document.getElementById("bgContainer")
+let bgColorTextEl = document.getElementById("bgColorText")
+
+
+const btn1 = () => {
+    bgContainerEl.style.backgroundColor = '#e0e0e0';
+    bgColorTextEl.textContent = '#e0e0e0';
+    bgColorTextEl.style.color = '#e0e0e0';
+}
+
+const btn2 = () => {
+    bgContainerEl.style.backgroundColor = '#6fcf97';
+    bgColorTextEl.textContent = '#6fcf97';
+    bgColorTextEl.style.color = '#6fcf97';
+}
+
+const btn3 = () => {
+    bgContainerEl.style.backgroundColor = '#56ccf2';
+    bgColorTextEl.textContent = '#56ccf2';
+    bgColorTextEl.style.color = '#56ccf2';
+}
+
+const btn4 = () => {
+    bgContainerEl.style.backgroundColor = '#bb6bd9';
+    bgColorTextEl.textContent = '#bb6bd9';
+    bgColorTextEl.style.color = '#bb6bd9';
+}
+
+
+
+
